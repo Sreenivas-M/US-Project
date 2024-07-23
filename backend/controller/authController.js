29a5308b0cfe8410919ad8cba2a92a57384c3108
Sreenivas-M <sreenivasmadala1991@gmail.com>
2024-07-23 20:40:23 +0530

    login,getall api

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,5 +1,8 @@
 const userModel = require('../model/userModel');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
+
+const TOKEN = process.env.TOKEN
 
 const createUser = async(req,res) =>{
     try {
@@ -19,6 +22,28 @@ const createUser = async(req,res) =>{
         console.error(error)
     }
 }
+const loginUser = async (req, res) => {
+    try {
+      const { email, password } = req.body;
+  
+      const user = await userModel.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ msg: "User not found!" });
+      }
+  
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ msg: "Invalid credentials!" });
+      }
+      const token = jwt.sign({user}, TOKEN, { expiresIn : "1d"})
+  
+      res.status(200).json({ token: token });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ msg: "Server error" });
+    }
+  };
+  
 
 const updateUser = async (req, res)=>{
     try {
@@ -44,6 +69,30 @@ const getUser = async (req, res)=>{
         res.status(200).json({msg:"Something went wrong"})
     }
 }
+const getAllUser = async (req, res) => {
+    const search = req.query.search || "";
+    const page = parseInt(req.params.page) || 1;
+    const perPage = parseInt(req.params.perPage) || 10;
+    const searchRegex = new RegExp(search, "i");
+    try {
+      const count = await userModel.countDocuments({
+        $or: [{ name: { $regex: searchRegex } }]
+      });
+  
+      const users = await userModel.find({
+        $or: [{ name: { $regex: searchRegex } }]
+      })
+      .limit(perPage)
+      .skip(perPage * (page - 1))
+      .sort({ createdAt: 1 });
+  
+      res.status(200).json({ users, count });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ msg: "Server error" });
+    }
+  };
+  
 const deleteUser = async (req, res)=>{
     try {
         const {id} = req.params
@@ -55,4 +104,4 @@ const deleteUser = async (req, res)=>{
 }
 
 
-module.exports = {createUser, updateUser, getUser, deleteUser };
+module.exports = {createUser, updateUser, getUser, deleteUser, loginUser, getAllUser };
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -5,11 +5,15 @@ const {
   updateUser,
   getUser,
   deleteUser,
+  loginUser,
+  getAllUser
 } = require("../controller/authController");
 
 route.post("/create", createUser);
 route.put("/update/:id", updateUser);
 route.get("/get/:id", getUser);
 route.delete("/delete/:id", deleteUser);
+route.post("/login", loginUser);
+route.post("/getAll", getAllUser);
 
 module.exports = route;
